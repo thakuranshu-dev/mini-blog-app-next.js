14e79b9ccc61b5f7f11d48c510093a9bacba9706
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-09-26 03:39:43 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default () => {
+  return(
+    <footer className="text-sm text-center bg-gray-500">
+      Made with 🧡 by Anshu K. Thakur
+      <p>&copy; {new Date().getFullYear()} Mini Blog App</p>
+    </footer>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const pathname = usePathname();
+  const routes = [
+    {name: 'Home', href: '/'},
+    {name: 'About', href: '/about'},
+    {name: 'Blogs', href: '/blog'},
+  ]
+
+  return (
+    <nav className="fixed w-full h-8 inline-flex gap-6 p-1 pl-8 bg-gray-300">
+      {routes.map(route=>(
+        <Link 
+        key={route.href}
+        href={route.href}
+        className={`text-lg hover:text-blue-600 ${pathname === route.href?
+          'underline font-semibold text-blue-600' :
+          'text-blue-500'
+        }`}
+        >
+          {route.name}
+        </Link>
+      ))}
+    </nav>
+  )
+}
