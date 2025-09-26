c45b7f7ee497468bfb7eb4a467a28dfacf9cc320
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-09-26 05:30:59 +0530

    Update not-found.tsx

--- src/app/blog/[id]/not-found.tsx
+++ src/app/blog/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function BlogNotFound() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold text-red-600">404 - Blog Post Not Found</h1>
+    <div className="p-6 h-screen block content-center text-center ">
+      <h1 className="text-2xl font-bold text-red-500">404 - Blog Post Not Found 👻</h1>
       <p className="mt-2">The blog post you're looking for doesn't exist.</p>
     </div>
   );
